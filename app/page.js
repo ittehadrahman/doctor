142ff8ec3dd1b69d438bdeee5b9be5ebfc6a6fed
ittehadrahman <ittehadrahman@iut-dhaka.edu>
2025-10-20 16:20:28 +0600

    .

--- app/page.js
+++ app/page.js
@@ -1,27 +1,28 @@
 "use client";
-import Hero from '@/components/Hero';
-import Services from '@/components/Services';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import Biography from '@/components/Biography';
-import Appointment from '@/components/Appointment';
-import Publications from '@/components/Publication';
-import Gallery from '@/components/Gallery';
-import VideoGallery from '@/components/VideoGallery';
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Biography from "@/components/Biography";
+import Appointment from "@/components/Appointment";
+import Publications from "@/components/Publication";
+import Gallery from "@/components/Gallery";
+import VideoGallery from "@/components/VideoGallery";
 export default function Home() {
   return (
     <>
+      {/* Page Content */}
       <div className="min-h-screen">
-      <Navbar />
-      <Hero />
-      <Biography />
-      <Appointment />
-      <Services />
-      <Publications />
-      <Gallery />
-      <VideoGallery />
-      <Footer />
-    </div>
+        <Navbar />
+        <Hero />
+        <Biography />
+        <Appointment />
+        <Services />
+        <Publications />
+        <Gallery />
+        <VideoGallery />
+        <Footer />
+      </div>
     </>
   );
 }
